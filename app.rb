0397cc9247dd9c02935a4ad21f4eef0ddb0303ac
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-19 16:35:56 +0000

    Commit5

--- app.rb
+++ app.rb
@@ -2,36 +2,36 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:A_new_square_cal)
+  erb(:a_new_square_cal)
 end
 
 get("/square/new") do
-  erb(:A_new_square_cal)
+  erb(:a_new_square_cal)
 end
 
 get("/square/results") do
-  erb(:A_square_results)
+  erb(:a_square_results)
 end
 
 get("/square_root/new") do
-  erb(:B_new_square_root_cal)
+  erb(:b_new_square_root_cal)
 end
 
 get("/square_root/results") do
-  erb(:B_square_root_results)
+  erb(:b_square_root_results)
 end
 
 get("/payment/new") do
-  erb(:C_new_payment_cal)
+  erb(:c_new_payment_cal)
 end
 
 get("/payment/results") do
-  erb(:C_new_payment_results)
+  erb(:c_new_payment_results)
 end
 get("/random/new") do
-  erb(:D_new_random_cal)
+  erb(:d_new_random_cal)
 end
 
 get("/random/results") do
-  erb(:D_random_results)
+  erb(:d_random_results)
 end
