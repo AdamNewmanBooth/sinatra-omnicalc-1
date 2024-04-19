08077048bb1fade70518915f1f8bfd10041c4210
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-19 16:28:55 +0000

    Commit 4

--- app.rb
+++ app.rb
@@ -2,12 +2,36 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-"<h1>howdy</h1>"
+  erb(:A_new_square_cal)
 end
 
 get("/square/new") do
-  erb(:new_square_cal)
-end 
+  erb(:A_new_square_cal)
+end
+
 get("/square/results") do
-  erb(:square_results)
+  erb(:A_square_results)
+end
+
+get("/square_root/new") do
+  erb(:B_new_square_root_cal)
+end
+
+get("/square_root/results") do
+  erb(:B_square_root_results)
+end
+
+get("/payment/new") do
+  erb(:C_new_payment_cal)
+end
+
+get("/payment/results") do
+  erb(:C_new_payment_results)
+end
+get("/random/new") do
+  erb(:D_new_random_cal)
+end
+
+get("/random/results") do
+  erb(:D_random_results)
 end
